5f9ec59a83a770a0db145d52a7a6c2d946659954
ellreka <ellreka5364@gmail.com>
2020-12-23 03:53:21 +0900

    aa

---
+++ app.js
@@ -0,0 +1,21 @@
+const protect = require('static-auth')
+const safeCompare = require('safe-compare')
+
+/*
+ *
+ */
+
+const app = protect(
+  '/admin',
+  (username, password) =>
+    safeCompare(username, 'admin') && safeCompare(password, 'admin'),
+  {
+    directory: __dirname + '/storybook-static',
+    realm: 'vercel-basic-auth.node-static-auth',
+    onAuthFailed: (res) => {
+      res.end('Restricted area, please login (admin:admin).')
+    }
+  }
+)
+
+module.exports = app
